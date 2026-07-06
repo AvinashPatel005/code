66c6ba9718bd3e580cfacea3f1064edd70be3fc8
Avinash Patel <avinash.patel@azentio.com>
2026-07-06 16:01:16 +0530

    --

--- Sample.java
+++ Sample.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class Sample {
     private String password = "admin1213"; // hardcoded!
 
+    public String getPassword() {
+        return password;
+    }
     public void processData(String input) {
         String query = "SELECT * FROM users WHERE id = " + input; // SQL injection
         System.out.println(query);
